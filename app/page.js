1713079c7beeeb53cb5080926ccdeea355057952
ADMINASHU <kashutosh02@gmail.com>
2024-11-17 13:34:32 +0530

    all month done

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
-"use client"
-import { useEffect, useState } from 'react';
+"use client";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function DataPage() {
-
-
   useEffect(() => {
     const fetchCookies = async () => {
       const apiURL = "/api/cookies";
@@ -17,19 +15,25 @@ export default function DataPage() {
       }
     };
     const fetchData = async () => {
-      const apiURL2 = "/api/sync";
-      const payload = {
-        month: 11,
-        year: 2024,
-        region: "",
-        branch: "",
-        type: "All",
-        callstatus: "",
-      };
-      const cookies = await fetchCookies();
-      const send = { payload, cookies };
       try {
-        const response2 = await axios.post(apiURL2, send);
+        const apiURL2 = "/api/sync";
+        const cookies = await fetchCookies();
+        const promises = [];
+
+        for (let month = 8; month < 13; month++) {
+          const payload = {
+            month: month,
+            year: 2024,
+            region: "",
+            branch: "",
+            type: "All",
+            callstatus: "",
+          };
+          const send = { payload, cookies };
+          promises.push(axios.post(apiURL2, send));
+        }
+        const responses = await Promise.all(promises);
+     
         // setData(response2.data);
         return null;
         // console.log("page: " + data);
@@ -40,10 +44,100 @@ export default function DataPage() {
     fetchData();
   }, []);
 
-
   return (
     <main>
       <h1>Data from API</h1>
     </main>
   );
 }
+
+//...............................................
+
+// "use client";
+
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import TableDisplayAndFilters from "./(components)/tableDisplayAndFilters";
+// import DataExtractor from "./(components)/dataExtractor";
+// import TableView from "./(components)/TableView";
+
+// const DataPage = () => {
+//   const [data, setData] = useState(null);
+//   const [processedData, setProcessedData] = useState([]);
+
+//   useEffect(() => {
+//     const fetchCookies = async () => {
+//       const apiURL = "/api/webapp";
+//       try {
+//         const response = await axios.get(apiURL);
+//         // setCookie(response.data);
+//         return response.data;
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     };
+//     async function fetchData() {
+//       try {
+//         let data = [];
+//         const apiURL2 = "/api/serviceease";
+//         const cookies = await fetchCookies();
+//         const promises = [];
+
+//         for (let month = 8; month < 13; month++) {
+//           const payload = {
+//             month: month,
+//             year: 2024,
+//             region: "",
+//             branch: "",
+//             type: "All",
+//             callstatus: "",
+//           };
+//           const send = { payload, cookies };
+//           promises.push(axios.post(apiURL2, send));
+//         }
+
+//         const responses = await Promise.all(promises);
+//         responses.forEach((response) => {
+//           const arr = response.data;
+//           console.log(arr);
+//           data = [...data, ...arr];
+//         });
+
+//         setData(data);
+//       } catch (error) {
+//         console.error("Error fetching data:", error);
+//       }
+//     }
+
+//     fetchData();
+//   }, []);
+//   const selectedColumns = [16, 18, 3, 14, 15, 26, 20, 21, 22, 23, 24, 25, 27];
+//   const points = {
+//     Breakdown: {
+//       eng: { new: 0, pending: 1, closed: [2, 2.5, 3] },
+//       branch: { new: -0.1, pending: -0.05, closed: 0 },
+//       region: { new: 0, pending: 0, closed: 0 },
+//     },
+//     Installation: {
+//       eng: { new: 0, pending: 1, closed: [2, 2.5, 3] },
+//       branch: { new: 0, pending: -0.25, closed: 0.2 },
+//       region: { new: 0, pending: 0, closed: 0 },
+//     },
+//     Pm: {
+//       eng: { new: 0, pending: 1.5, closed: [2, 3, 3] },
+//       branch: { new: 0, pending: -0.5, closed: 0.1 },
+//       region: { new: 0, pending: 0, closed: 0 },
+//     },
+//   };
+//   return (
+//     <div>
+//       <h1>Complaint Data</h1>
+//       {/* <p>{JSON.stringify(data)}</p> */}
+
+//       <DataExtractor data={data} onDataProcessed={setProcessedData} />
+//       <TableView data={processedData} selectedColumns={selectedColumns} />
+//     </div>
+//   );
+// };
+
+// export default DataPage;
