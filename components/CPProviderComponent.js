36a406097e246a28aa9ff3f4e24bde0c46d40ecc
Ashutosh Kumar <kashutosh02@gmail.com>
2025-02-18 15:14:21 +0530

    run

--- components/CPProviderComponent.js
+++ components/CPProviderComponent.js
@@ -79,7 +79,7 @@ export const CPProviderComponent = ({ children }) => {
       const cookiesResponse = await axios.post("/api/get-cookies");
       const promises = [];
 
-      for (let i = 10000; i < 20000; i++) {
+      for (let i = 45000; i < 50000; i++) {
         const payload = { id: i };
 
         const promise = axios
